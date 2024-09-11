c2e34f6d4200f10be85f58af370732ff139bc599
Thomas3246 <anatoliy0642751@mail.ru>
2024-09-11 22:51:59 +0300

    v2 | most functions

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"testTask/src/handle"
 	"testTask/src/service"
@@ -9,16 +8,15 @@ import (
 
 func main() {
 
-	// Добавить куки
-
-	loggedUser := service.Login("FirstUser", "Password1")
-	fmt.Print(loggedUser)
-
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	http.HandleFunc("/notes/", handle.MainHandler)
-	http.HandleFunc("/notes/login", handle.LoginHandler)
+	http.HandleFunc("/notes/login/", handle.LoginHandlerPage)
+	http.HandleFunc("/notes/logout/", handle.LogoutHandler)
+	http.HandleFunc("/notes/add/", handle.AddNoteHandler)
+	http.HandleFunc("/notes/confirm/", handle.ConfirmNoteHandler)
+	http.HandleFunc("/notes/mynotes/", handle.MyNotesHandler)
 
 	err := http.ListenAndServe("localhost:8080", nil)
 	service.Check(err)
--- src/handle/handlers.go
+++ src/handle/handlers.go
@@ -1,11 +1,83 @@
 package handle
 
-import "net/http"
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"testTask/src/service"
+)
+
+var user service.User
 
 func MainHandler(writer http.ResponseWriter, request *http.Request) {
-
+	htmlfile, err := template.ParseFiles("templates/mainView.html")
+	service.Check(err)
+	err = htmlfile.Execute(writer, nil)
+	service.Check(err)
 }
 
-func LoginHandler(writer http.ResponseWriter, request *http.Request) {
+func LoginHandlerPage(writer http.ResponseWriter, request *http.Request) {
+	var output string
+	var err error
 
+	fmt.Println(request.Method)
+
+	if request.Method == http.MethodPost {
+		login := request.FormValue("login")
+		password := request.FormValue("password")
+
+		user, err = service.Login(login, password)
+		if err != nil {
+			output = "Неверый логин или пароль"
+		} else {
+			http.Redirect(writer, request, "/notes/", http.StatusFound)
+		}
+	}
+
+	htmlfile, err := template.ParseFiles("templates/loginView.html")
+	service.Check(err)
+	err = htmlfile.Execute(writer, output)
+	service.Check(err)
+}
+
+func MyNotesHandler(writer http.ResponseWriter, request *http.Request) {
+	htmlfile, err := template.ParseFiles("templates/myNotesView.html")
+	service.Check(err)
+
+	if user.GetLogin() == "" {
+		var answer []string
+		nonAuth := "Вы не авторизованы"
+		answer = append(answer, nonAuth)
+
+		err = htmlfile.Execute(writer, answer)
+		service.Check(err)
+	} else {
+		myNotes := user.GetNotes()
+		err = htmlfile.Execute(writer, myNotes)
+		service.Check(err)
+	}
+}
+
+func AddNoteHandler(writer http.ResponseWriter, request *http.Request) {
+	htmlfile, err := template.ParseFiles("templates/addView.html")
+	service.Check(err)
+	err = htmlfile.Execute(writer, user.GetLogin())
+	service.Check(err)
+}
+
+func ConfirmNoteHandler(writer http.ResponseWriter, request *http.Request) {
+	newNote := request.FormValue("newNoteInput")
+	if newNote == "" {
+		http.Redirect(writer, request, "/notes/add/", http.StatusFound)
+	} else {
+		fmt.Println(newNote)
+		// Добавить фичу яндекса
+		// Добавить новую заметку через u.AddNote()
+		http.Redirect(writer, request, "/notes/", http.StatusFound)
+	}
+}
+
+func LogoutHandler(writer http.ResponseWriter, request *http.Request) {
+	user = user.Logout()
+	http.Redirect(writer, request, "/notes/login/", http.StatusFound)
 }
